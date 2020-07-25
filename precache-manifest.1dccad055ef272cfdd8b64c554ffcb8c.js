1dc670507ce373f2964f09393852943afd974d7a
chirag <chirag@chirag.com>
2020-07-25 19:49:19 +0530

    Updates

---
+++ precache-manifest.1dccad055ef272cfdd8b64c554ffcb8c.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76ee91dbb0d191890aacfc3c5e4500bc",
+    "url": "/MiteshKalal7/reactjs/index.html"
+  },
+  {
+    "revision": "bab9b0c9d8b85e8ba5e3",
+    "url": "/MiteshKalal7/reactjs/static/css/2.12264522.chunk.css"
+  },
+  {
+    "revision": "0edb038a5db39f60ee90",
+    "url": "/MiteshKalal7/reactjs/static/css/main.0def0d83.chunk.css"
+  },
+  {
+    "revision": "bab9b0c9d8b85e8ba5e3",
+    "url": "/MiteshKalal7/reactjs/static/js/2.cb0219ab.chunk.js"
+  },
+  {
+    "revision": "00684873e44470594fbda565b2a6be21",
+    "url": "/MiteshKalal7/reactjs/static/js/2.cb0219ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0edb038a5db39f60ee90",
+    "url": "/MiteshKalal7/reactjs/static/js/main.a1aaccd7.chunk.js"
+  },
+  {
+    "revision": "50df81c0d5b27d27f2e5",
+    "url": "/MiteshKalal7/reactjs/static/js/runtime-main.a5bbf8c5.js"
+  },
+  {
+    "revision": "73476783ca0f7583198978de7073815b",
+    "url": "/MiteshKalal7/reactjs/static/media/0.73476783.jpg"
+  },
+  {
+    "revision": "ed986061ad9bf89c081724362956b134",
+    "url": "/MiteshKalal7/reactjs/static/media/1.ed986061.jpeg"
+  },
+  {
+    "revision": "fe349e8528c79623ea6795ddd110985a",
+    "url": "/MiteshKalal7/reactjs/static/media/10.fe349e85.jpeg"
+  },
+  {
+    "revision": "780ccb41c83dad3cbd24a9bcd3262586",
+    "url": "/MiteshKalal7/reactjs/static/media/2.780ccb41.jpeg"
+  },
+  {
+    "revision": "41bf3241965cc7c2990174ed9d6e9714",
+    "url": "/MiteshKalal7/reactjs/static/media/3.41bf3241.jpeg"
+  },
+  {
+    "revision": "026cb7af3133d721efff2a0134f7e16f",
+    "url": "/MiteshKalal7/reactjs/static/media/4.026cb7af.jpeg"
+  },
+  {
+    "revision": "6e846951d7a04c931a0f9efa94a1316f",
+    "url": "/MiteshKalal7/reactjs/static/media/5.6e846951.jpeg"
+  },
+  {
+    "revision": "47789d678cfff97c48fda4de3f3621d5",
+    "url": "/MiteshKalal7/reactjs/static/media/6.47789d67.jpeg"
+  },
+  {
+    "revision": "2835396c930c8bf53ffd3beeedba7c41",
+    "url": "/MiteshKalal7/reactjs/static/media/7.2835396c.jpeg"
+  },
+  {
+    "revision": "10b775cc930b6986ec69eb9a4adef56a",
+    "url": "/MiteshKalal7/reactjs/static/media/8.10b775cc.jpeg"
+  },
+  {
+    "revision": "565ad4a387d2090e5d73f0c7b8de2793",
+    "url": "/MiteshKalal7/reactjs/static/media/9.565ad4a3.jpeg"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/MiteshKalal7/reactjs/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "60a859917fe65ea1f9f070e0dc080c76",
+    "url": "/MiteshKalal7/reactjs/static/media/shards-dashboards-logo.60a85991.svg"
+  }
+]);
